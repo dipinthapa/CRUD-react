6e94a8c9544567bf634319fd336b344f8a2ecee2
unknown <dipin.thapa1@gmail.com>
2026-03-23 18:32:27 +0545

    view user slicing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import User from './components/create-user/User'
+import View from './components/view-user/View'
 
 export default function App() {
   return (
-    <User />
+    <><User /><View /></>
   )
 }
---
+++ src/api/api.js
@@ -0,0 +1,5 @@
+const getPosts = async() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    method: "GET",
+    
+}
